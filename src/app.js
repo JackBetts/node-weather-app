82317a44cc3e227cc670f22a0adf09e194aa3c13
JackBetts <42355170+JackBetts@users.noreply.github.com>
2020-12-07 23:31:13 +0000

    Mucking about

--- src/app.js
+++ src/app.js
@@ -46,19 +46,19 @@ app.get('/help', (req, res) => {
 
 app.get('/weather', (req, res) => {
 
-    if(!req.query.address){
+    if (!req.query.address) {
         return res.send({
             error: 'You must provide an address to find the weather'
         })
     }
 
     geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-        if(error) {
+        if (error) {
             return res.send({ error })
         }
 
         forecast(latitude, longitude, (error, forecastData) => {
-            if(error) {
+            if (error) {
                 return res.send({ error })
             }
             res.send({
@@ -71,7 +71,7 @@ app.get('/weather', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-    if(!req.query.search) {
+    if (!req.query.search) {
         return res.send({
             error: 'You must provide a search term'
         })
